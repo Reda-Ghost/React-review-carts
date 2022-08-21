e8c953771ba357d3515e19f6a93d74f951ba0dd8
Reda-Ghost <86271659+Reda-Ghost@users.noreply.github.com>
2022-08-21 19:41:53 +0000

    final project

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Review from './Review';
+function App() {
+	return (
+		<main>
+			<section>
+				<div className="container">
+					<div className="title">
+						<h2>Our Reviews</h2>
+						<div className="underline"></div>
+          </div>
+          <Review />
+				</div>
+			</section>
+		</main>
+	);
+}
+
+export default App;
